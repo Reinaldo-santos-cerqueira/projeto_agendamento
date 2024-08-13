2c2cf6ddbc946ef9030bc559562c7f89002ffd25
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-13 08:42:31 -0300

    feat: implementando login e funções basicas para usuario

---
+++ app/Models/User.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $keyType = 'string';
+    protected $fillable = [
+        'nome', 'identificador', 'senha', 'tipo',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+
+    protected $hidden = [
+        'senha',
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['senha'] = bcrypt($value);
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->senha;
+    }
+}
