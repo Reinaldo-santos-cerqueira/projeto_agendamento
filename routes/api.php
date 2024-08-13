2ebf51277adcc9d3b6e658ee0abe16b5a6cbbbdb
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-13 08:44:30 -0300

    feat: configurando as rotas de usuario, auth e agendamentos

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AgendamentoController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+//auth
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
+
+//users
+Route::post('users', [UserController::class, 'store']);
+Route::middleware('auth:sanctum')->get('users/pacientes', [UserController::class, 'getUsuariosTipoP']);
+Route::middleware('auth:sanctum')->get('users', [UserController::class, 'index']);
+Route::middleware('auth:sanctum')->get('users/{id}', [UserController::class, 'show']);
+Route::delete('users/{id}', [UserController::class, 'destroy']);
+Route::put('users/{id}', [UserController::class, 'update']);
+
+//agendamentos
+Route::middleware('auth:sanctum')->get('agendamentos/{id}', [AgendamentoController::class, 'show']);
+Route::middleware('auth:sanctum')->post('agendamentos', [AgendamentoController::class, 'store']);
+Route::middleware('auth:sanctum')->get('agendamentos', [AgendamentoController::class, 'getAll']);
+Route::middleware('auth:sanctum')->put('agendamentos/{id}', [AgendamentoController::class, 'update']);
